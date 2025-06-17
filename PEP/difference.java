527ab5352ae022df6f0a759078ac5e33e7f06111
Muskan <muskansharma0203@gmail.com>
2025-06-17 09:05:18 +0530

    changes

---
+++ PEP/difference.java
@@ -0,0 +1,13 @@
+package PEP;
+import java.util.Scanner;
+public class difference {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+        int res=(a*b)-(c*d);
+        System.out.println(res);
+    }
+}
