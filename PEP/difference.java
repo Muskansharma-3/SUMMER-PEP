b789866d8d34b8495aa2486f43a27d60a67070f9
Muskan <muskansharma0203@gmail.com>
2025-06-17 12:48:04 +0530

    change

--- PEP/difference.java
+++ PEP/difference.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class difference {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a= sc.nextInt();
-        int b=sc.nextInt();
-        int c=sc.nextInt();
-        int d=sc.nextInt();
-        int res=(a*b)-(c*d);
-        System.out.println(res);
+        long A= sc.nextInt();
+        long B=sc.nextInt();
+        long C=sc.nextInt();
+        long D=sc.nextInt();
+        long res=(A*B)-(C*D);
+        System.out.println("Difference = "+res);
     }
 }
