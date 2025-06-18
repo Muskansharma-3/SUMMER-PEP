691b93a5c7d598673b43f6f9f6c9abf13d9d907e
Muskan <muskansharma0203@gmail.com>
2025-06-18 11:43:05 +0530

    changes

---
+++ PEP/constructSum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class constructSum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        int[][] arr=new int[T][2];
+        for(int i=0;i<T;i++){
+            arr[i][0]=sc.nextInt();
+            arr[i][1]=sc.nextInt();
+        }
+        
+    }
+}
