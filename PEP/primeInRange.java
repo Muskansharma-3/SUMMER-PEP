4c605c15ec8e739a034d5dbf7a69ab5feca7ae16
Muskan <muskansharma0203@gmail.com>
2025-06-18 10:22:51 +0530

    changes

--- PEP/primeInRange.java
+++ PEP/primeInRange.java
@@ -4,5 +4,20 @@ public class primeInRange {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int N=sc.nextInt();
+        for(int i=1;i<=N;i++){
+            if(isPrime(i)){
+                System.out.println(i);
+            }
+        }
+    }
+    static boolean isPrime(int n){
+        if(n<2) return false;
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                return false;
+            }
+            
+        }
+        return true;
     }
 }
