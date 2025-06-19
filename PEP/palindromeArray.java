d03bb6b65f173975a4b8a02914b4ade2248696a2
Muskan <muskansharma0203@gmail.com>
2025-06-19 11:23:21 +0530

    changes

--- PEP/palindromeArray.java
+++ PEP/palindromeArray.java
@@ -4,9 +4,24 @@ public class palindromeArray {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int N=sc.nextInt();
-        long[] arr=new long[N];
+        int[] arr=new int[N];
         for(int i=0;i<N;i++){
-            arr[i]=sc.nextLong();
+            arr[i]=sc.nextInt();
+        }
+        int start=0;
+        int end=arr.length-1;
+        boolean palindrome=false;
+        while(start<end){
+            if(arr[start]!=arr[end]){
+                System.out.println("NO");
+                palindrome=true;
+                break;
+            }
+            start++;
+            end--;
+        }
+        if(!palindrome){
+            System.out.println("YES");
         }
         
     }
---
+++ PEP/rangeSumquery.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class rangeSumquery {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int Q=sc.nextInt();
+        long[] arr=new long[N+1];
+        for(int i=1;i<=N;i++){
+            arr[i]=sc.nextLong();
+        }
+        
+        long[] prefix=new long[N+1];
+        for(int i=1;i<=N;i++){
+            prefix[i]=prefix[i-1]+arr[i];
+        }
+        while(Q-- > 0){
+            int L=sc.nextInt();
+            int R=sc.nextInt();
+            System.out.println(prefix[R] - prefix[L - 1]);      
+        }
+
+    }
+}
