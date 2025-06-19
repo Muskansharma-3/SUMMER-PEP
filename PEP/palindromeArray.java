4008f605d6e3fb87833fc7a4a40d1ca6e40e117c
Muskan <muskansharma0203@gmail.com>
2025-06-19 10:10:21 +0530

    changes

--- PEP/palindromeArray.java
+++ PEP/palindromeArray.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
 
-public class reverseArray {
+public class palindromeArray {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int N=sc.nextInt();
         long[] arr=new long[N];
         for(int i=0;i<N;i++){
-            arr[i]=sc.nextInt();
+            arr[i]=sc.nextLong();
         }
-        for(int i=N-1;i>=0;i--){
-            System.out.print(arr[i]+" ");
-        }
+        
     }
 }
