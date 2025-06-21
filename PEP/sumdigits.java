f8ff97c3625c7229ccd437e89186edd90149df18
Muskan <muskansharma0203@gmail.com>
2025-06-21 16:21:00 +0530

    change

--- PEP/sumdigits.java
+++ PEP/sumdigits.java
@@ -2,8 +2,18 @@ import java.util.Scanner;
 
 public class sumdigits {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int N=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();           // Read number of digits
+        String digits = sc.next();      // Read the digits as a string
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += digits.charAt(i) - '0';  // Convert char digit to int and add
+        }
+
+        System.out.println(sum);
+
         
     }
 }
