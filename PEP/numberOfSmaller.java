941c01b21c783f389c727d616082c9bc1c30b660
Muskan <muskansharma0203@gmail.com>
2025-06-23 10:43:33 +0530

    change

--- PEP/numberOfSmaller.java
+++ PEP/numberOfSmaller.java
@@ -13,5 +13,28 @@ public class numberOfSmaller {
         for(int i=0;i<m;i++){
             b[i]=sc.nextLong();
         }
+
+        // brute force -> n*m
+        // for(int j=0;j<m;j++){
+        //     int count=0;
+        //     for(int i=0;i<n;i++){
+        //         if(a[i]<b[j]) count++;
+        //     }
+        //     System.out.print(count+" ");
+        // }
+
+
+        //optimised
+        int i=0;
+        int count=0;
+        for(int j=0;j<m;j++){
+            while(i<n){
+                if(a[i]<b[j]) count++;
+                else break;
+                i++;
+            }
+            System.out.print(count+" ");
+        }
+
     }
 }
