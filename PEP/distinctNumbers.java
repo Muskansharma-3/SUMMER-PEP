4613c6eff894816929983da98bb26b5ae253f2b5
Muskan <muskansharma0203@gmail.com>
2025-06-24 09:30:13 +0530

    changes

--- PEP/distinctNumbers.java
+++ PEP/distinctNumbers.java
@@ -8,6 +8,14 @@ public class distinctNumbers {
         for(int i=0;i<n;i++){
             a[i]=sc.nextLong();
         }
-        
+        Arrays.sort(a);
+        int i=1;
+        for(int j=1;j<n;j++){
+            if(a[j]!=a[j-1]){
+                a[i]=a[j];
+                i++;
+            }
+        }
+        System.out.println(i);
     }
 }
