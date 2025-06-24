779bc3e9d271cccd3f9d34cb38e1b11deb387be3
Muskan <muskansharma0203@gmail.com>
2025-06-24 09:16:18 +0530

    changes

--- PEP/distinctNumbers.java
+++ PEP/distinctNumbers.java
@@ -1,18 +1,12 @@
 import java.util.Scanner;
 
-public class apartments {
+public class distinctNumbers {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int m=sc.nextInt();
-        long k=sc.nextLong();
         long[] a=new long[n];
         for(int i=0;i<n;i++){
             a[i]=sc.nextLong();
-        }
-        long[] b=new long[m];
-        for(int i=0;i<m;i++){
-            b[i]=sc.nextLong();
         }
         
     }
