4acc52b9ab823f1c388b27146724fde99c5adfa2
Muskan <muskansharma0203@gmail.com>
2025-06-25 09:25:33 +0530

    changes

---
+++ PEP/minSubarraySum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class minSubarraySum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        long[] a=new long[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextLong();
+        }
+        long sum=0;
+        long min=Integer.MAX_VALUE;
+        for(long num:a){
+            sum+=num;
+            min=Math.min(min, sum);
+            if(sum > 0) sum = 0;
+        }
+        System.out.println(min);
+    }
+}
