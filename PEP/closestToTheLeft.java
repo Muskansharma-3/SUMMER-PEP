20de11e9da32d31d5d2d3415046356b5b130c822
Muskan <muskansharma0203@gmail.com>
2025-07-01 10:59:05 +0530

    change

--- PEP/closestToTheLeft.java
+++ PEP/closestToTheLeft.java
@@ -5,9 +5,13 @@ public class closestToTheLeft {
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
         int k=sc.nextInt();
-        int[] arr=new int[n];
+        long[] arr=new long[n];
         for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+            arr[i]=sc.nextLong();
+        }
+        long[] que=new long[k];
+        for(int i=0;i<k;i++){
+            que[i]=sc.nextLong();
         }
         
     }
