eddecd9b1f16d8fb4fddf6a9909bceeeb3d64eb3
Muskan <muskansharma0203@gmail.com>
2025-07-01 11:34:35 +0530

    change

---
+++ PEP/packingRectangles.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class packingRectangles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long w=sc.nextLong();
+        long h=sc.nextLong();
+        long n=sc.nextLong();
+    }
+}
