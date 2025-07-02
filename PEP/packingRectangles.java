1e42958d595782c850bc87beaaa8b2b3871cca0b
Muskan <muskansharma0203@gmail.com>
2025-07-02 10:52:28 +0530

    change

--- PEP/packingRectangles.java
+++ PEP/packingRectangles.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
 
 public class packingRectangles {
+    public static boolean check(long x, long w, long h, long n){
+        return (x/w)*(x/h)>=n;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long w=sc.nextLong();
         long h=sc.nextLong();
         long n=sc.nextLong();
-        long low=1;
-        long high=n*n;
-        long ans=0;
-        while(low<high){
-            long mid=low+(high-low)/2;
-            if((mid/w)*(mid/h)>=n){
-                ans=mid;
-                high=mid-1;
+        long low=0;
+        long high=1;
+        while(!check(high,w,h,n)) high*=2;
+        while(high-low>1){
+            long mid=(low+high)>>1;
+            if(check(mid, w, h, n)){
+                high=mid;
             }else{
-                low=mid+1;
+                low=mid;
             }
         }  
-        System.out.println(ans);
+        System.out.println(high);
     }
 }
