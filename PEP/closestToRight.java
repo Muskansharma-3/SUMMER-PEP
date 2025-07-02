f651863257cd0618796c310a71002da420d5d1fc
Muskan <muskansharma0203@gmail.com>
2025-07-02 09:09:20 +0530

    changes

--- PEP/closestToRight.java
+++ PEP/closestToRight.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class closestToTheLeft {
+public class closestToRight {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
@@ -13,22 +13,17 @@ public class closestToTheLeft {
             long x=sc.nextLong();
             int left=0;
             int right=n-1;
-            int ans=-1;
+            int ans=n;
             while(left<=right) {
                 int mid=(left+right)/2;
-                if(arr[mid]<=x){
+                if(arr[mid]>=x){
                     ans=mid;
-                    left=mid+1;
-                }else{
                     right=mid-1;
+                }else{
+                    left=mid+1;;
                 }
             }
-            if(ans==-1){
-                System.out.println(0);
-            }else{
-                System.out.println(ans+1);
-            }
+            System.out.println(ans+1);
         }
         }
 }
-
