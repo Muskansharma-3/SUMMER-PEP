43191809dca18a6b2f1fb7e1ee3f158275e5e319
Muskan <muskansharma0203@gmail.com>
2025-07-03 09:41:32 +0530

    change

--- PEP/equation.java
+++ PEP/equation.java
@@ -1,8 +1,22 @@
 import java.util.Scanner;
 public class equation {
+    public static boolean fun(double x, double C){
+        return (x*x)+Math.sqrt(x)>=C;
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        long C=sc.nextLong();
-        
+        double C=sc.nextDouble();
+        double x=1.0;
+        System.out.format("%");
+        double i=1, j=C;
+        while(j-i>0.0000001){
+            double mid=(double)(i+j)/(2.0);
+            if(fun(mid, C)){
+                j=mid;
+            }else{
+                i=mid;
+            }
+        }
+        System.out.println(j);
     }
 }
