6568f4215df75a59fca0f2be7f61dc9545ce76fd
Muskan <muskansharma0203@gmail.com>
2025-07-03 10:10:49 +0530

    chnge

--- PEP/veryEasyTask.java
+++ PEP/veryEasyTask.java
@@ -6,6 +6,23 @@ public class veryEasyTask {
         int n=sc.nextInt();
         int x=sc.nextInt();
         int y=sc.nextInt();
-        
+        if (n==1) {
+            System.out.println(Math.min(x, y));
+            return;
+        }
+        int first=Math.min(x, y);
+        n--;
+        long low=0, high=1L*n*Math.max(x, y), ans=0;
+        while (low<=high) {
+            long mid=(low+high)/2;
+            long total=(mid/x)+(mid/y);
+            if (total>=n) {
+                ans=mid;
+                high=mid-1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        System.out.println(ans + first);
     }
 }
