e2579c54d20d01ff2bb56b775c67689535defa42
Muskan <muskansharma0203@gmail.com>
2025-07-03 11:41:20 +0530

    changes

---
+++ PEP/brackets.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class brackets {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        Stack<Character> stack=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='(') stack.push(ch);
+            else stack.pop();
+        }
+        if(stack.isEmpty()){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+    }
+}
