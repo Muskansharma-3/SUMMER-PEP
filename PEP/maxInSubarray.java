3c09e06cff6dbe573f75e95f36574b662c13d075
Muskan <muskansharma0203@gmail.com>
2025-07-04 11:30:38 +0530

    changes

--- PEP/maxInSubarray.java
+++ PEP/maxInSubarray.java
@@ -9,11 +9,11 @@ public class maxInSubarray{
             arr[i]=sc.nextInt();
         }
         int k=sc.nextInt();
-        for(int i=0;i<=n-k;i++){
+        for(int i=0;i<n-k+1;i++){
             int max=arr[i];
-            for(int j=i;j<i+k;j++){
-                if(arr[j]>max){
-                    max=arr[j];
+            for(int j=1;j<k;j++){
+                if(arr[i+j]>max){
+                    max=arr[i+j];
                 }
             }
             System.out.print(max+" ");
