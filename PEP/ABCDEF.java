4d316e0be82f9d87d0c6045bb75f5cccc01a3ca7
Muskan <muskansharma0203@gmail.com>
2025-07-04 10:55:46 +0530

    change

---
+++ PEP/ABCDEF.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ABCDEF {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] s=new int[n];
+        for(int i=0;i<n;i++){
+            s[i]=sc.nextInt();
+        }
+        
+    }
+}
