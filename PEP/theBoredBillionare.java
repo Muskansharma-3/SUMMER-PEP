687161799f609b5cb6b19e28aa476c8b62d3351e
Muskan <muskansharma0203@gmail.com>
2025-07-04 10:41:10 +0530

    change

---
+++ PEP/theBoredBillionare.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class theBoredBillionare {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        while(T-->0){
+            long N=sc.nextLong();
+            
+            
+        }
+    }
+}
