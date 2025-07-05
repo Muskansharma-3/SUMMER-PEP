45496e2a1660fd505976b1d43c09ff213bdc42c4
Muskan <muskansharma0203@gmail.com>
2025-07-05 14:37:04 +0530

    change

--- PEP/theBoredBillionare.java
+++ PEP/theBoredBillionare.java
@@ -7,8 +7,20 @@ public class theBoredBillionare {
         while(T-->0){
             long N=sc.nextLong();
             int i=0;
-            
-            
+            long low=0;
+            long high=(long) Math.sqrt(2*N)+1;
+            long res=0;
+            while(low<=high){
+                long mid=low+(high-low)/2;
+                long total=(mid*(mid+1))/2;
+                if(total<=N){
+                    res=mid;
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
+            System.out.println(res);
         }
     }
 }
