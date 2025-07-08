aa7c02b15ab49544d2f2d614ca58183105057d21
Muskan <muskansharma0203@gmail.com>
2025-07-08 11:42:22 +0530

    change

---
+++ PEP/factorial2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class factorial2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(factorial(n));
+    }
+    private static int factorial(int n){
+        if(n==0) return 0;
+    }
+}
