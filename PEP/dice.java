c36bf3e317402a4c659419997c5ca17fc1538207
Muskan <muskansharma0203@gmail.com>
2025-07-08 10:54:36 +0530

    change

--- PEP/dice.java
+++ PEP/dice.java
@@ -9,6 +9,11 @@ public class dice {
     private static int print(int n){
         if(n==0) return 1;
         if(n<0) return 0;
-        return(print(n-1)+print(n-2)+print(n-3)+print(n-4)+print(n-5)+print(n-6));
+        int ans=0;
+        for(int i=1;i<=6;i++){
+            ans+=print(n-i);
+        }
+        return ans;
+        // return(print(n-1)+print(n-2)+print(n-3)+print(n-4)+print(n-5)+print(n-6));
     }
 }
