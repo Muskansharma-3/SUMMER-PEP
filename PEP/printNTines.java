0209585999c45cc07aacedbe8fcb925362898acd
Muskan <muskansharma0203@gmail.com>
2025-07-08 10:32:50 +0530

    change

--- PEP/printNTines.java
+++ PEP/printNTines.java
@@ -4,6 +4,11 @@ public class printNTines {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int N=sc.nextInt();
-        
+        print(N);
+    }
+    private static void print(int N){
+        if(N==0) return;
+        System.out.println("I love Recursion");
+        print(N-1);
     }
 }
