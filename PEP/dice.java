6e167bc69fa3fe063b295b47daf389e5a74fe9f9
Muskan <muskansharma0203@gmail.com>
2025-07-08 10:51:23 +0530

    change

--- PEP/dice.java
+++ PEP/dice.java
@@ -4,6 +4,11 @@ public class dice {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        
+        System.out.println(print(n));
+    }
+    private static int print(int n){
+        if(n==0) return 1;
+        if(n<0) return 0;
+        return(print(n-1)+print(n-2)+print(n-3)+print(n-4)+print(n-5)+print(n-6));
     }
 }
---
+++ PEP/printNTo1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class printNTo1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        print(n);
+    }
+    private static void print(int n){
+        if(n<=0) return;
+        System.out.print(n+" ");
+        print(n-1);
+    }
+}
