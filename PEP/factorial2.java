ab23ad14fb09be20112f47570a042327c276e972
Muskan <muskansharma0203@gmail.com>
2025-07-09 09:06:15 +0530

    change

--- PEP/factorial2.java
+++ PEP/factorial2.java
@@ -7,6 +7,7 @@ public class factorial2 {
         System.out.println(factorial(n));
     }
     private static int factorial(int n){
-        if(n==0) return 0;
+        if(n==0 || n==1) return 1;
+        return n*factorial(n-1);
     }
 }
