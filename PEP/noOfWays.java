a10c5315e88d56a30ff067deb32834219c8def05
Muskan <muskansharma0203@gmail.com>
2025-07-09 10:08:28 +0530

    change

--- PEP/noOfWays.java
+++ PEP/noOfWays.java
@@ -5,6 +5,11 @@ public class noOfWays {
         Scanner sc=new Scanner(System.in);
         int s=sc.nextInt();
         int e=sc.nextInt();
-        
+        System.out.println(count(s,e));
+    }
+    private static int count(int s,int e){
+        if(s==e) return 1;
+        if(s>e) return 0;
+        return count(s+1, e)+count(s+2,e)+count(s+3,e);
     }
 }
