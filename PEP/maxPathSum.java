36b6d11107f3ff02cbc346f6ba98169b73caca04
Muskan <muskansharma0203@gmail.com>
2025-07-10 09:28:19 +0530

    change

--- PEP/maxPathSum.java
+++ PEP/maxPathSum.java
@@ -17,8 +17,8 @@ public class maxPathSum {
     private static int find(int[][] arr, int i, int j, int n, int m){
         if(i>=n || j>=m) return Integer.MIN_VALUE;
         if(i==n-1 && j==m-1) return arr[i][j];
-        int a=find(arr,i,j+1,n,m);
-        int b=find(arr, i+1,j,n,m);
-        return arr[i][j]+Math.max(a,b);
+        int right=find(arr,i,j+1,n,m);
+        int down=find(arr, i+1,j,n,m);
+        return arr[i][j]+Math.max(right,down);
     }
 }
