66732175cacc64bc0a58b2f7dae1faf48be85638
Muskan <muskansharma0203@gmail.com>
2025-07-11 11:29:13 +0530

    change

--- PEP/bonusExp.java
+++ PEP/bonusExp.java
@@ -8,6 +8,17 @@ public class bonusExp {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextLong();
         }
-        
+        System.out.println(find(arr,0,n, 0));
     }
+    private static int find(long[] arr, int i, int n, int count){
+        if(i>=n) return 0;
+        int nottake=find(arr,i+1,n,count);
+        int take=Integer.MIN_VALUE;
+        if(count%2==0){
+            take=(int)arr[i]+find(arr,i+1,n, count+1);
+        }else{
+            take=(int)(arr[i]+arr[i]+find(arr,i+1,n, count+1));
+        }
+        return Math.max(take, nottake)
+;    }
 }
