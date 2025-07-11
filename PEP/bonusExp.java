9ac41e6658ef877576902b14c7a68481f98db01b
Muskan <muskansharma0203@gmail.com>
2025-07-11 11:34:23 +0530

    change

--- PEP/bonusExp.java
+++ PEP/bonusExp.java
@@ -1,6 +1,7 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class bonusExp {
+    static int[][]dp;
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
@@ -8,17 +9,21 @@ public class bonusExp {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextLong();
         }
+        dp=new int[n][2];
+        Arrays.fill(dp,-1);
         System.out.println(find(arr,0,n, 0));
     }
     private static int find(long[] arr, int i, int n, int count){
         if(i>=n) return 0;
+        if(dp[i][count]!=-1) return dp[i][count];
         int nottake=find(arr,i+1,n,count);
         int take=Integer.MIN_VALUE;
         if(count%2==0){
-            take=(int)arr[i]+find(arr,i+1,n, count+1);
+            take=(int)arr[i]+find(arr,i+1,n, 1);
         }else{
-            take=(int)(arr[i]+arr[i]+find(arr,i+1,n, count+1));
+            take=(int)(arr[i]+arr[i]+find(arr,i+1,n, 0));
         }
-        return Math.max(take, nottake)
-;    }
+        dp[i][count]=Math.max(take, nottake);
+        return dp[i][count];
+    }
 }
