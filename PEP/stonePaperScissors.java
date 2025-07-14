7a7d4aaaaa23540df768ba9a6b0214fd09e08d11
Muskan <muskansharma0203@gmail.com>
2025-07-14 10:04:42 +0530

    change

--- PEP/stonePaperScissors.java
+++ PEP/stonePaperScissors.java
@@ -5,6 +5,29 @@ public class stonePaperScissors {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         String s=sc.nextLine();
-        
+        int rock = 0, scissors = 0, paper = 0;
+
+        for (char c : S.toCharArray()) {
+            int prevRock = rock, prevScissors = scissors, prevPaper = paper;
+
+            // Update all based on previous state (not repeating the last move)
+            rock = Math.max(prevScissors, prevPaper);
+            scissors = Math.max(prevRock, prevPaper);
+            paper = Math.max(prevRock, prevScissors);
+
+            if (c == 'R') {
+                scissors = 0;
+                paper++;
+            } else if (c == 'S') {
+                paper = 0;
+                rock++;
+            } else if (c == 'P') {
+                rock = 0;
+                scissors++;
+            }
+        }
+
+        int result = Math.max(rock, Math.max(scissors, paper));
+        System.out.println(result);
     }
 }
