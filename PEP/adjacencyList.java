2b658ea073558dc33bf33bfde24523d42592bb68
Muskan <muskansharma0203@gmail.com>
2025-07-17 09:42:43 +0530

    change

--- PEP/adjacencyList.java
+++ PEP/adjacencyList.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class adjacencyList {
-    
+    public static void main(String[] args) {
+        int V=4;
+        List<List<Integer>> list=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            list.add(new ArrayList<>());
+        }
+        addEdge(list,0,1);
+        addEdge(list,0,2);
+        addEdge(list,1,2);
+        addEdge(list,2,3);
+        display(list);
+    }
+    private static void addEdge(List<List<Integer>> list, int i, int j){
+        list.get(i).add(j);
+        list.get(j).add(i);
+    }
 }
