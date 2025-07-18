8c1536f2ab48c96fbdf0eab65d3119fdd9f83cfa
Muskan <muskansharma0203@gmail.com>
2025-07-18 11:10:52 +0530

    change

---
+++ PEP/findTheDilema.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class findTheDilema {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        long m=sc.nextLong();
+        long x=sc.nextLong();
+        long y=sc.nextLong();
+        int k=sc.nextInt();
+        long[] arr=new long[k];
+        for(int i=0;i<k;i++){
+            arr[i]=sc.nextLong();
+        }
+    }
+}
