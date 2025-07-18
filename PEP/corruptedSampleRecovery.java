5807df61103c50ec879ef4502e3f9388f6c8c6f0
Muskan <muskansharma0203@gmail.com>
2025-07-18 10:08:38 +0530

    change

--- PEP/corruptedSampleRecovery.java
+++ PEP/corruptedSampleRecovery.java
@@ -4,9 +4,9 @@ public class corruptedSampleRecovery {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int[] arr=new int[n];
+        long[] arr=new long[n];
         for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+            arr[i]=sc.nextLong();
         }
     }
 }
